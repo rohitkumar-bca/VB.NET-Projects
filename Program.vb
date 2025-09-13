9a70ab54a9c340f5e46d5493eac8635757977043
ROHIT KUMAR <gamingislove2020@gmail.com>
2025-09-13 23:28:26 +0530

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,90 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        'Console.WriteLine("Hello World!")
+
+        'Dim marks As Integer
+
+        'Console.WriteLine("enter your marks")
+        'marks = Convert.ToInt32(Console.ReadLine())
+
+        'If marks > 33 Then
+        '    Console.WriteLine("You are pass")
+        'End If
+
+
+
+        'If marks > 33 Then
+        '    Console.WriteLine("You are pass")
+        'Else
+        '    Console.WriteLine("You are fail")
+        'End If
+
+
+
+        'If marks >= 90 Then
+        '    Console.WriteLine("You got A1")
+        'ElseIf marks >= 80 Then
+        '    Console.WriteLine("You got A2")
+        'ElseIf marks >= 70 Then
+        '    Console.WriteLine("You got B1")
+        'ElseIf marks >= 60 Then
+        '    Console.WriteLine("You got B2")
+        'ElseIf marks >= 50 Then
+        '    Console.WriteLine("You got C1")
+        'ElseIf marks >= 40 Then
+        '    Console.WriteLine("You got C2")
+        'ElseIf marks >= 33 Then
+        '    Console.WriteLine("You got D")
+        'Else
+        '    Console.WriteLine("You are fail")
+        'End If
+
+
+        'Dim day As Integer
+        'Console.WriteLine("Enter day number (1-7):")
+        'day = Convert.ToInt32(Console.ReadLine())
+
+        'Select Case day
+        '    Case 1
+        '        Console.WriteLine("Monday")
+        '    Case 2
+        '        Console.WriteLine("Tuesday")
+        '    Case 3
+        '        Console.WriteLine("Wednesday")
+        '    Case 4
+        '        Console.WriteLine("Thursday")
+        '    Case 5
+        '        Console.WriteLine("Friday")
+        '    Case 6
+        '        Console.WriteLine("Saturday")
+        '    Case 7
+        '        Console.WriteLine("Sunday")
+        '    Case Else
+        '        Console.WriteLine("Invalid day number")
+        'End Select
+
+
+        'Dim age As Integer
+        'Console.WriteLine("Enter your age:")
+        'age = Convert.ToInt32(Console.ReadLine())
+
+        'Dim citizenship As Boolean
+        'Console.WriteLine("Are you a citizen of India? (True/False):")
+        'citizenship = Convert.ToBoolean(Console.ReadLine())
+
+        'If age >= 18 Then
+        '    If citizenship = True Then
+        '        Console.WriteLine("You are eligible to vote.")
+        '    Else
+        '        Console.WriteLine("You are not able to vote.")
+        '    End If
+
+        'End If
+
+
+
+
+    End Sub
+End Module
